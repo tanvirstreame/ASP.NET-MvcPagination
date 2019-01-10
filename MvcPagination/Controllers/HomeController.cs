f91d3f31c1e361960293b9598dd2ec370eaf57e2
tanvirstreame <streametanvir@gmail.com>
2019-01-10 20:56:37 +0600

    updated

--- MvcPagination/Controllers/HomeController.cs
+++ MvcPagination/Controllers/HomeController.cs
@@ -25,47 +25,10 @@ namespace MvcPagination.Controllers
 
         };
         // GET: Home
-        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        public ViewResult Index(int? page)
         {
-            ViewBag.CurrentSort = sortOrder;
-            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
-
-            if (searchString != null)
-            {
-                page = 1;
-            }
-            else
-            {
-                searchString = currentFilter;
-            }
-
-            ViewBag.CurrentFilter = searchString;
-
-            var students = from s in StudentDb
-                           select s;
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                students = students.Where(s => s.LastName.Contains(searchString)
-                                       || s.FirstName.Contains(searchString));
-            }
-            switch (sortOrder)
-            {
-                case "name_desc":
-                    students = students.OrderByDescending(s => s.LastName);
-                    break;
-                case "Date":
-                    students = students.OrderBy(s => s.EnrollDate);
-                    break;
-                case "date_desc":
-                    students = students.OrderByDescending(s => s.EnrollDate);
-                    break;
-                default:  // Name ascending 
-                    students = students.OrderBy(s => s.LastName);
-                    break;
-            }
-
-            int pageSize = 3;
+            var students = from s in StudentDb select s;
+            int pageSize = 4;
             int pageNumber = (page ?? 1);
             return View(students.ToPagedList(pageNumber, pageSize));
         }
